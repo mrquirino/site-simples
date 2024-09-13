d5b2d9e5363acb926cecaa38b9fc2724f6588d27
Matheus Quirino <matheusf730@gmail.com>
2024-09-13 12:58:19 -0300

    up

--- app.js
+++ app.js
@@ -4,4 +4,13 @@ const switcher = document.querySelector('.btn');
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if (className == "ligth-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Ligth"
+    }
+    console.log('current class name: ' + className);
 });
